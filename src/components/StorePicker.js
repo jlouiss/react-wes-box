7000e69a1a325709a3840fefc11acddc6ca2d640
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2020-06-09 22:16:52 +0100

    [04] - Writing HTML with JSX

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello!</p>;
+    return (
+      <form className="store-selector">
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Please enter a store"/>
+        <button type="submit">Visit store -></button>
+      </form>
+    );
   }
 }
 
